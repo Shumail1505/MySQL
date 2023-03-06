7d9adbbdb1e4300f342e90222a283994678e7cfb
Shumail <118796798+Shumail1505@users.noreply.github.com>
2023-03-06 23:39:09 +0530

    Update Project.sql

--- Project.sql
+++ Project.sql
@@ -2,25 +2,32 @@ create DATABASE healthcare;
 
 use  healthcare;
 
+
 select * from data;
+
 
 ALTER TABLE data
 DROP COLUMN datacol;
 
+
 ALTER TABLE data
 ADD CONSTRAINT Customer_ID PRIMARY KEY (id);
+
 
 SELECT AVG(Age) AS avg_age, AVG(children) AS avg_children, AVG(BMI) AS avg_bmi, AVG(charges) AS avg_hosp_costs
 FROM data
 WHERE HBA1C > 6.5 AND heart_problems = 'yes';
 
+
 SELECT Hospital_tier, City_tier, AVG(charges) AS avg_cost
 FROM data
 GROUP BY Hospital_tier, City_tier;
 
+
 SELECT COUNT(*) AS patients
 FROM data
 WHERE Heart_Issues = 'yes' AND cancer_history = 'Yes';
+
 
 SELECT state, COUNT(*) AS num_hospitals
 FROM data
@@ -28,6 +35,7 @@ WHERE hospital_tier = 'tier - 1'
 GROUP BY state;
 
 
+select * from data;
 
 
 
