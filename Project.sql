b1a2073c28cc7a769967ac24e41434f1953e5ee7
Shumail <118796798+Shumail1505@users.noreply.github.com>
2023-03-11 11:37:26 +0530

    Update Project.sql

--- Project.sql
+++ Project.sql
@@ -1,44 +1,104 @@
-create DATABASE healthcare;
+Create database employee;
 
-use  healthcare;
+SELECT  EMP_ID, FIRST_NAME, LAST_NAME, GENDER,DEPT
+FROM emp_record_table;
 
+SELECT  EMP_ID, FIRST_NAME, LAST_NAME, GENDER,DEPT,EMP_RATING  FROM emp_record_table
+WHERE EMP_RATING<2;
+SELECT  EMP_ID, FIRST_NAME, LAST_NAME, GENDER,DEPT,EMP_RATING  FROM emp_record_table
+WHERE EMP_RATING>4;
+SELECT  EMP_ID, FIRST_NAME, LAST_NAME, GENDER,DEPT,EMP_RATING  FROM emp_record_table
+WHERE EMP_RATING BETWEEN 2 AND 4;
 
-select * from data;
+SELECT CONCAT(FIRST_NAME,'',LAST_NAME) AS 
+NAME FROM emp_record_table
+WHERE DEPT = "FINANCE";
 
+SELECT m.EMP_ID,m.FIRST_NAME,m.LAST_NAME,m.ROLE,
+m.EXP,COUNT(e.EMP_ID) as "EMP_COUNT"
+FROM emp_record_table m
+INNER JOIN emp_record_table e
+ON m.EMP_ID = e.MANAGER_ID
+GROUP BY m.EMP_ID
+ORDER BY m.EMP_ID;
 
-ALTER TABLE data
-DROP COLUMN datacol;
+SELECT m.EMP_ID,m.FIRST_NAME,m.LAST_NAME,m.ROLE,
+m.EXP,COUNT(e.EMP_ID) as "EMP_COUNT"
+FROM emp_record_table m
+INNER JOIN emp_record_table e
+ON m.EMP_ID = e.MANAGER_ID
+GROUP BY m.EMP_ID
+ORDER BY m.EMP_ID;
 
+SELECT  m.EMP_ID,m.FIRST_NAME,m.LAST_NAME,m.ROLE,m.DEPT,m.EMP_RATING,max(m.EMP_RATING)
+OVER(PARTITION BY m.DEPT)
+AS "MAX_DEPT_RATING"
+FROM emp_record_table m
+ORDER BY DEPT;
 
-ALTER TABLE data
-ADD CONSTRAINT Customer_ID PRIMARY KEY (id);
+SELECT EMP_ID, FIRST_NAME, LAST_NAME, ROLE, MAX(SALARY), MIN(SALARY)
+FROM emp_record_table
+WHERE ROLE IN("PRESIDENT","LEAD DATA SCIENTIST","SENIOR DATA SCIENTIST","MANAGER","ASSOCIATE DATA SCIENTIST","JUNIOR DATA SCIENTIST")
+GROUP BY ROLE;
 
+SELECT EMP_ID,FIRST_NAME,LAST_NAME,EXP,
+RANK() OVER(ORDER BY EXP) EXP_RANK
+FROM emp_record_table;
 
-SELECT AVG(Age) AS avg_age, AVG(children) AS avg_children, AVG(BMI) AS avg_bmi, AVG(charges) AS avg_hosp_costs
-FROM data
-WHERE HBA1C > 6.5 AND heart_problems = 'yes';
+CREATE VIEW employees_in_various_countries AS
+SELECT EMP_ID,FIRST_NAME,LAST_NAME,COUNTRY,SALARY
+FROM emp_record_table
+WHERE SALARY>6000;
 
+SELECT *FROM employees_in_various_countries;
 
-SELECT Hospital_tier, City_tier, AVG(charges) AS avg_cost
-FROM data
-GROUP BY Hospital_tier, City_tier;
+SELECT EMP_ID,FIRST_NAME,LAST_NAME,EXP FROM emp_record_table
+WHERE EMP_ID IN(SELECT manager_id FROM emp_record_table);
 
+DELIMITER &&
+CREATE PROCEDURE get_experience_details()
+BEGIN
+SELECT EMP_ID,FIRST_NAME,LAST_NAME,EXP FROM emp_record_table WHERE EXP>3;
+END &&
+CALL get_experience_details();
 
-SELECT COUNT(*) AS patients
-FROM data
-WHERE Heart_Issues = 'yes' AND cancer_history = 'Yes';
+DELIMITER &&
+CREATE FUNCTION Employee_ROLE(
+EXP int
+)
+RETURNS VARCHAR(40)
+DETERMINISTIC
+BEGIN
+DECLARE Employee_ROLE VARCHAR(40);
+IF EXP>12 AND 16 THEN
+SET Employee_ROLE="MANAGER";
+ELSEIF EXP>10 AND 12 THEN
+SET Employee_ROLE ="LEAD DATA SCIENTIST";
+ELSEIF EXP>5 AND 10 THEN
+SET Employee_ROLE ="SENIOR DATA SCIENTIST";
+ELSEIF EXP>2 AND 5 THEN
+SET Employee_ROLE ="ASSOCIATE DATA SCIENTIST";
+ELSEIF EXP<=2 THEN
+SET Employee_ROLE ="JUNIOR DATA SCIENTIST";
+END IF;
+RETURN (Employee_ROLE);
+END &&
 
+SELECT EXP,Employee_ROLE(EXP)
+FROM data_science_team;
 
-SELECT state, COUNT(*) AS num_hospitals
-FROM data
-WHERE hospital_tier = 'tier - 1'
-GROUP BY state;
+update emp_record_table set salary=(select salary +(select salary*.05*EMP_RATING))
+SELECT *FROM emp_record_table;
 
+update emp_record_table set salary=(select salary +(select salary*.05*EMP_RATING))
+SELECT *FROM emp_record_table;
 
-select * from data;
-
-
-
+SELECT EMP_ID,FIRST_NAME,LAST_NAME,SALARY,COUNTRY,CONTINENT,
+AVG(salary)OVER(PARTITION BY COUNTRY)AVG_salary_IN_COUNTRY,
+AVG(salary)OVER(PARTITION BY CONTINENT)AVG_salary_IN_CONTINENT, 
+COUNT(*)OVER(PARTITION BY COUNTRY)COUNT_IN_COUNTRY,
+COUNT(*)OVER(PARTITION BY CONTINENT)COUNT_IN_CONTINENT
+     FROM emp_record_table;
 
 
 
